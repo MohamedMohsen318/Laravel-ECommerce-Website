9a86ac9a34ff70370eee1830d5c5ffad33e73185
Mohamed Mohsen <Mohamedsn101@gmail.com>
2026-06-14 03:09:58 +0300

    Redesign welcome store page

--- app/Http/Controllers/User/CategoryController.php
+++ app/Http/Controllers/User/CategoryController.php
@@ -10,21 +10,18 @@ class CategoryController extends Controller
 {
     public function __construct(
         protected CategoryService $categoryService
-    ) {}
+    ) {
+        //
+    }
 
-    public function index()
-    {
+    public function index(){
         $categories = $this->categoryService->getRootCategories();
-
         return view('user.categories.index', compact('categories'));
     }
 
-    public function show(string $path)
-    {
+    public function show(string $path){
         $category = $this->categoryService->findByPath($path);
-
         $products = new LengthAwarePaginator([], 0, 12);
-
         return view('user.categories.show', compact('category', 'products'));
     }
 }
--- app/Services/User/CategoryService.php
+++ app/Services/User/CategoryService.php
@@ -6,24 +6,19 @@ use App\Models\Category;
 
 class CategoryService
 {
-    public function getRootCategories()
-    {
+    public function getRootCategories(){
         return Category::whereNull('parent_id')
             ->where('is_active', true)
             ->with('children')
             ->get();
     }
-
-    public function findByPath(string $path): Category
-    {
+    public function findByPath(string $path): Category{
         $slugs = explode('/', $path);
         $slug = end($slugs);
-
         $category = Category::where('slug', $slug)
             ->where('is_active', true)
             ->with('children')
             ->firstOrFail();
-
         return $category;
     }
 }
